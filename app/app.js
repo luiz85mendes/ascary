8b1c17d6aefec9a7c56b0378a45c931ded87ba88
Luiz Mendes <luiz85mendes@gmail.com>
2018-05-29 16:42:54 -0300

    form-control no formulario

--- app/app.js
+++ app/app.js
@@ -3,10 +3,18 @@
 
   angular.module("app", []).controller("IndexController", function($scope) {
     $scope.titulo = "Teste2";
-  
+
+  var parametro:  
+  $scope.selectBrand = function (nome) {
+    $scope.lista = [
+      {"marca": 1, "name": "BBS"},
+      {"marca": 2, "name": "TSW"}
+    
+    ]
+
+  } 
+
   });
-  
-  
 
 })();
 
