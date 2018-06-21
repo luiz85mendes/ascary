64b342ec5cf3cba9ae4314efe9878e1b35ed6853
Luiz Mendes <luiz85mendes@gmail.com>
2018-06-21 16:36:02 -0300

    ultimo commit branas

--- app/guardado/app.js
+++ app/guardado/app.js
@@ -1,23 +1,42 @@
+(function (){
+
+
 var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.
-    when('/', { controller: 'listController', templateUrl: 'search.html', resolve: { Lista: } }).
+    when('/', { controller: 'listController', templateUrl: 'search.html' }).
     when('/edit/:name', { controller: 'editController', templateUrl: 'form.html' }).
     when('/new', { controller: 'newController', templateUrl: 'form.html' }).otherwise({ redirectTo: '/' });
 }]);
 
 
 app.run(['$rootScope', function ($rootScope) {
-  $rootScope.fruits = ["banana", "apple", "orange"]; //Variável fruits possui contexto global ao módulo.
+//Variável fruits possui contexto global ao módulo.
   console.log('app.run');
-
-
+ 
+  
+  
 }]);
 
-app.controller('listController', function($scope) {
-  console.log('bateu aqui');
-});
+app.controller('listController', ['$scope', function ($scope) {
+  $scope.nomes = ['teste'];
+  $scope.fruits = ["banana", "apple", "orange"]; 
+  
+  $scope.findCityByState = function (estado) {
+    $scope.cidades = [];
+    if (!estado) return;
+    commonService.findByStateOrderCapital(estado).then(function (response) {
+        $scope.cidades = response.data.data;
+    });
+  };
+
+  this.findStates = function() {
+    return $http.get('base/common/state');
+  };
+
+}]);  
+
 
 
 
@@ -57,4 +76,4 @@ app.controller('newController', function ($scope, $location, $routParams) {
 });
 
 
-
+})();
