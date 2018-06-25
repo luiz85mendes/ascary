cd1198ae0b49936cf30efcab874ea954b354677c
Luiz Mendes <luiz.mendes@squadra.com.br>
2018-06-25 17:53:11 -0300

    ultimo intregrando back

---
+++ app/http.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+var operadoras = [
+    { nome: "Vivo", codigo: 14, categoria: "Celular" },
+    { nome: "Tim", codigo: 41, categoria: "Celular" },
+    { nome: "Vivo", codigo: 15, categoria: "Celular" },
+    { nome: "GVT", codigo: 25, categoria: "Fixo" },
+    { nome: "Embratel", codigo: 21, categoria: "Fixo" }
+];
+
+var contatos = [
+    { nome: "Pedro", telefone: "9998888"},
+    { nome: "Ana", telefone: "9998877" },
+    { nome: "Maria", telefone: "9998866" }
+];
+
+
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.write(JSON.stringify(contatos));
+    res.end();
+}).listen(8082);
