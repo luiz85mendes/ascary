9cc3dd713d7192dda56824cfa64864c8ced10349
Luiz Mendes <luiz.mendes@squadra.com.br>
2018-07-09 18:24:59 -0300

    rest

--- app/app.js
+++ app/app.js
@@ -1,19 +1,39 @@
-var app = angular.module('app', ['ui.router']);
+(function () {
+    'use strict';
 
+    var homeResolve = {
+        parametros: ['HomeService', '$message', '$filter', function (homeService, $message, $filter) {
+            return homeService.findAll().then(function (response) {
+                var parametros = response.data.data;
+                return parametros; 
+            }, function (response) {
+                $message.addMsgDanger(response.message);
+                return [];
+            });
+        }],
+    }
 
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise('/');
+    angular.module('app', ['ui.router']);
 
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: 'home.html',
-            controller: 'HomeController'
+    angular.module('app').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise('/');
+        
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home.html',
+                controller: 'HomeController',
+            });
+    });
+
+    angular.module('app').controller('HomeController',['$scope', '$http', function ($scope, $http) {
+        $scope.teste;
+        $http.get('http://localhost:8080/wheels/buscar').then(function (response) {
+            $scope.teste = response.data;
+
         });
-});
+        $scope.message = 'teste';
+       
+    }]);
 
-app.controller('HomeController', function ($scope) {
-    $scope.message = "teste";
-});
-
-app.service('HomeService')
+})();
