5cb459d9873094f662d83b5ca64118d0fb8ed79d
Luiz Mendes <luiz.mendes@squadra.com.br>
2018-08-13 17:31:58 -0300

    commit ng-options

--- app/app.js
+++ app/app.js
@@ -5,13 +5,13 @@
 
     angular.module('app').config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
         $urlRouterProvider.otherwise('/');
-
+        
         $stateProvider.state('home', {
             url: '/',
             templateUrl: 'home.html',
             controller: 'HomeController',
             resolve: {
-
+              
             }
         }).state('search', {
             url: '/search',
@@ -22,22 +22,10 @@
 
     });
 
-    angular.module('app').controller('HomeController', ['$scope', '$http', '$state', '$stateParams', '$anchorScroll', function ($scope, $http, $state, $stateParams, $anchorScroll) {
-
-        $http.get('http://localhost:8080/wheels/buscar').then(function (response) {
-            $scope.parametros = response.data;
-
-        });
-
-        $scope.searchWheels = function () {
-            $http.get('http://localhost:8080/wheels/buscar').then(function (response) {
-                $scope.parametros = response.data;
-                $location.hash('anchor');
-                $anchorScroll();
-
-            });
-        }
+    angular.module('app').controller('Principal', ['$scope', '$http', function ($scope, $http) {
+        $scope.principal = "pagina principal";
 
     }]);
+    
 
 })();
--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,22 +1,33 @@
 (function () {
     'use strict';
 
-    angular.module('app').controller('HomeController', ['$scope', '$http', function ($scope, $http, $state, $stateParams, $anchorScroll) {
+    angular.module('app').controller('HomeController', ['$scope', '$http',  '$state', '$stateParams', '$anchorScroll',  function ($scope, $http, $state, $stateParams, $anchorScroll) {
 
+      $scope.menu = "Buscar";
+      $scope.parametros = [];
         
         $http.get('http://localhost:8080/wheels/buscar').then(function (response) {
             $scope.parametros = response.data;
-
+            console.log($scope.parametros);
+           
         });
 
         $scope.searchWheels = function () {
             $http.get('http://localhost:8080/wheels/buscar').then(function (response) {
-                $scope.parametros = response.data;
+                $scope.para = response.data;
                
-              
-
             });
         }
+
+        $scope.items = [{
+            id: 1,
+            label: 'aLabel',
+            subItem: { name: 'aSubItem' }
+          }, {
+            id: 2,
+            label: 'bLabel',
+            subItem: { name: 'bSubItem' }
+          }];
 
     }]);
 
